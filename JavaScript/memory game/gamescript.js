cb8684fbac365da8bf1be4b12c0a5139767eafbc
achadaev <andrew.chadaev@gmail.com>
2019-07-09 08:36:42 +0300

    Added 'memory game' realization

---
+++ JavaScript/memory game/gamescript.js
@@ -0,0 +1,74 @@
+var opened = 0;
+var count = 16;
+var first;
+
+var dict = {
+	'00': '3',
+	'10': '6',
+	'20': '8',
+	'30': '1',
+	'01': '4',
+	'11': '7',
+	'21': '8',
+	'31': '5',
+	'02': '2',
+	'12': '4',
+	'22': '1',
+	'32': '3',
+	'03': '6',
+	'13': '5',
+	'23': '2',
+	'33': '7'
+};
+
+function hide(card0, card1)
+{
+	card0.src = 'images/hidden.jpg'
+	card1.src = 'images/hidden.jpg'
+}
+
+function del(card0, card1)
+{
+    card0.parentNode.parentNode.deleteCell(card0.parentNode.cellIndex);
+	card1.parentNode.parentNode.deleteCell(card1.parentNode.cellIndex);
+}
+
+function check(card0, card1)
+{
+	if (dict[card0.id.slice(4)] == dict[card1.id.slice(4)])
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+function show(thisCard)
+{
+	var path = 'images/' + dict[thisCard.id.slice(4)] + '.jpg';
+	thisCard.src = path;
+	opened += 1;
+	if (opened == 1)
+	{
+		first = thisCard;
+	}
+	else if (opened == 2)
+	{
+		if (check(thisCard, first))
+		{
+			setTimeout(del, 2000, thisCard, first);
+			count -= 2;
+		}
+		else
+		{
+			setTimeout(hide, 2000, thisCard, first);
+		}
+		opened = 0;
+	}
+	if (count == 0)
+		{
+			document.getElementById('title').innerHTML = 'Good job!';
+		}
+}
